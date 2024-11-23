c1c991c731c2737de4d96beb1136d2acec0bdb72
John <johncruzag@gmail.com>
2024-11-23 17:09:55 -0500

    Updated Certification Component to include new PJPT Cert

--- src/components/certficiations/Certficiations.jsx
+++ src/components/certficiations/Certficiations.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './certifications.css';
 import { FaExternalLinkAlt, FaCertificate } from 'react-icons/fa';
-import pjwtLogo from '../../assets/pjwtLogo.png'
+import pjwtLogo from '../../assets/pjwtLogo.png';
 import csLogo from '../../assets/csLogo.png';
+import pjptLogo from '../../assets/pjptLogo.png'; // Import PJPT logo
 
 const certifications = [
   {
@@ -23,8 +24,21 @@ const certifications = [
     description: 'Focused on hands-on penetration testing for web applications, including reconnaissance and exploitation.',
     logo: pjwtLogo, // Reference to the PJWT badge
   },
+  {
+    id: 3,
+    title: 'Practical Junior Penetration Tester (PJPT)',
+    issuer: 'TCM Security',
+    date: 'Issued 10/15/2024',
+    url: 'https://certified.tcm-sec.com/006bf430-1660-4ee2-adda-d5cda8608055?key=723d1af6dc8c0bf85589342089df6c945f6f917d16ce5a7ad7cfb69a0b84fd3f',
+    description: `This exam assessed the ability to perform an internal network penetration test at an associate level, including:
+      - Ethical hacking
+      - Penetration testing
+      - Active Directory exploitation
+      - Enumeration
+      - Information gathering`,
+    logo: pjptLogo, // Reference to the PJPT badge
+  },
 ];
-
 
 const Certifications = () => {
   return (
@@ -32,21 +46,21 @@ const Certifications = () => {
       <h3><FaCertificate /> Certifications</h3>
       <ul className="certifications__list">
         {certifications.map(({ id, title, issuer, date, url, description, logo }) => (
-         <li key={id} className="certifications__item">
-         <div className="certifications__badge-container">
-           <img src={logo} alt={`${title} badge`} className="certifications__badge" />
-         </div>
-         <h4>{title}</h4>
-         <p>{issuer} - {date}</p>
-         <p>{description}</p>
-         <div className="certifications__links">
-           {url && (
-             <a href={url} target="_blank" rel="noopener noreferrer" className="btn">
-               View Online <FaExternalLinkAlt />
-             </a>
-           )}
-         </div>
-       </li>       
+          <li key={id} className="certifications__item">
+            <div className="certifications__badge-container">
+              <img src={logo} alt={`${title} badge`} className="certifications__badge" />
+            </div>
+            <h4>{title}</h4>
+            <p>{issuer} - {date}</p>
+            <p>{description}</p>
+            <div className="certifications__links">
+              {url && (
+                <a href={url} target="_blank" rel="noopener noreferrer" className="btn">
+                  View Online <FaExternalLinkAlt />
+                </a>
+              )}
+            </div>
+          </li>
         ))}
       </ul>
     </div>
