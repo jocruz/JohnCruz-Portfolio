d3de3d4776a5b47b116c7f24e39060765fe300eb
John <johncruzag@gmail.com>
2025-03-09 03:57:24 -0400

    added another CTA button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,6 +4,9 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href={CV} download className='btn'> Download CV </a>
+        <a href="https://johncruz.info/appsec" className='btn btn-primary'>
+        View Web App Sec Gitbook
+      </a>
     </div>
   )
 }
