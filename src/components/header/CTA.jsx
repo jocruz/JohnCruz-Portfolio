fbbdf838db266a09f9b0787e5edf94e236294786
John <johncruzag@gmail.com>
2025-03-09 04:00:36 -0400

    changed css to CTA buttons

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'> Download CV </a>
+        <a href={CV} download className='btn btn-primary'> Download CV </a>
         <a href="https://johncruz.info/appsec" className='btn btn-primary'>
         View Web App Sec Gitbook
       </a>
